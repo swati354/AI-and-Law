11bc5a205e4449adca55c22e62099582355ef2b3
Swati Tiwari <swatitiwari354@gmail.com>
2023-06-26 12:54:42 +0530

    adding files to the repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const[previousChats, setPreviousChats] = useState([]);
+  const[currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () =>{
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  }
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  }
+
+  const getMessages = async () => {
+    const options = {
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json"
+      },
+      body : JSON.stringify({
+        message : value
+      })
+    }
+    try{
+        const response = await fetch('http://localhost:8000/completions', options);
+        const data = await response.json();
+        setMessage(data.choices[0].message);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  // console.log(message);
+  //whenever message changes this will run
+  useEffect(() => {
+    // console.log(currentTitle, value, message);
+    if(!currentTitle && value && message)
+    {
+      setCurrentTitle(value);
+    }
+    if(currentTitle && value && message)
+    {
+      setPreviousChats(previousChats => (
+        //open previousChats and add two objects
+      [...previousChats, {
+          title: currentTitle,
+          role : "user",
+          content : value
+      },{
+        title: currentTitle,
+        role : message.role,
+        content : message.content
+      }]
+      ))
+    }
+  }, [message, currentTitle])
+
+  console.log(previousChats);
+
+  const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle);
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)));
+  console.log(uniqueTitles);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+      <section className="side-bar">
+        <button onClick = {createNewChat}>+ New chat</button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle,index) => <li key = {index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+        </ul>
+        <nav>
+          <p>Happy searching</p>
+        </nav>
+      </section>
+
+      <section className="main">
+        {/* it will be displayed only when there is no currentTitle  */}
+        {!currentTitle && <h1>MyGPT</h1>}
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => <li key = {index}>
+            <p className="role">{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+        <div className="bottom-container">
+          <div className="input-container">
+            {/* initially value of the input would be value which initially is null  */}
+            {/* e is the event  */}
+            <input value = {value} onChange={(e) => setValue(e.target.value)}/>
+            <div id = "submit" onClick={getMessages}>&#10146;</div>
+          </div>
+          <p className="info">
+          Free Research Preview. ChatGPT may produce inaccurate information about people, places, or facts. ChatGPT May 24 Version
+          </p>
+        </div>
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
