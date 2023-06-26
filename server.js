6c3316ec897119af55ba3404a003b982dd6873b3
swati354 <103816801+swati354@users.noreply.github.com>
2023-06-26 12:57:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const PORT = 8000;
+
+const app = express();
+//we cant parse over json from the frontend to the backend unless
+//we have this 
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = process.env.API_KEY;
+
+app.post('/completions', async (req,res) => {
+    const options = {
+        method : "POST",
+        headers: {
+            "Authorization" : `Bearer ${API_KEY}`,
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify({
+            model : "gpt-3.5-turbo",
+            messages: [{role : "user", content: req.body.message}],
+            max_tokens : 100
+        })
+    }
+    try{
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+        const data = await response.json()
+        res.send(data);
+    }catch(error){
+        console.error(error);
+    }
+})
+
+app.listen(PORT, () => console.log('Your server is running on PORT ' + PORT));
